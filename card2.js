68e7376289cef4ffc8c37a20b6f1219171e2a29f
rachel519 <rachelgr519@gmail.com>
2024-05-02 07:05:36 -0600

    Actualizacion del ejercicio

--- card2.js
+++ card2.js
@@ -8,7 +8,8 @@
 */
 
 
-const USER = [
+const CARDS_CONTAINER = document.querySelector("#card-container");
+const USERS = [
     {
     id: 1,
     username: "User Name",
@@ -39,13 +40,48 @@ const USER = [
     }
 ]
 
-const CARDS_CONTAINER = document.querySelector("#card-container");
-
-USER.forEach(USER => {
-    const tarjeta = document.createElement("div")
-    tarjeta.innerHTML=
-    CARDS_CONTAINER.appendChild(tarjeta)
-}
-);
+// Crear secciones
+function createElements() {
+    const cardObj = {
+      name_section: document.createElement("h3"),
+      desc_section: document.createElement("p"),
+      age_section: document.createElement("p"),
+      book_section: document.createElement("div"),
+    };
+    return cardObj;
+  }
+  
+  // Crear tarjeta
+  // Inyectamos info
+  function injectData(obj, user) {
+    const bookList = user.fav_books.books.map((e) => {
+      const item = document.createElement("ul");
+      item.textContent = e;
+      return item;
+    });
+  
+    obj.name_section.textContent = user.username;
+    obj.desc_section.textContent = user.desc;
+    obj.age_section.textContent = user.age;
+    obj.book_section.append(...bookList);
+    renderCard(obj);
+  }
+  
+  function renderCard(cardObj) {
+    const card = document.createElement("div");
+    card.append(
+      cardObj.name_section,
+      cardObj.desc_section,
+      cardObj.age_section,
+      cardObj.book_section,
+    );
+    card.className = "div-creado";
+    CARDS_CONTAINER.appendChild(card);
+  }
+  
+  USERS.forEach((user) => {
+    const cardData = createElements();
+    injectData(cardData, user);
+  });
 
 
